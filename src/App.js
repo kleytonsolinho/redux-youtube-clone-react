c2cd6ff4f4e1647a26964f980016565b2b0365c4
Kleyton Solinho <kleytonsolinho@gmail.com>
2021-10-10 21:12:12 -0300

    feat: add initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,3 @@
-import { useState } from 'react';
-
 export default function App() {
-  const [user, setUser] = useState('1');
-
-  setUser('Testando!');
-  return (
-    <h1>
-      Hello World!
-      {user}
-    </h1>
-  );
+  return <h1>Hello World!</h1>;
 }
